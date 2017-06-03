30b7f41d7086b0af2ee2abfbc8271d9676f6b7a1
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2017-06-04 00:16:12 +0200

    prepare for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
+  'plugins': [
+    'jest'
+  ],
   'extends': [
     '@yepstr/eslint-config-yepstr/common',
     '@yepstr/eslint-config-yepstr/node',
   ],
+  'env': {
+    'jest/globals': true,
+  },
 };
